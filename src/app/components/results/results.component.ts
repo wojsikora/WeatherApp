901c42dd597fda07e2a5d15e9ba06376bfd5d5f2
wojsikora <wojciech.sikora.yt@gmail.com>
2022-04-19 23:01:27 +0200

    change style

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -19,7 +19,7 @@ export class ResultsComponent implements OnInit {
     humidity: '',
     weather: '',
     weather_desc: '',
-    wind_speed: ''
+    wind_speed: '',
   };
 
   constructor(
@@ -28,27 +28,34 @@ export class ResultsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.data.currenctMessage.subscribe((locationName) => (this.locationName = locationName));
-    this.weatherService.getLocationCoordinates(this.locationName).subscribe(
-      (data: any) => {
+    this.data.currenctMessage.subscribe(
+      (locationName) => (this.locationName = locationName)
+    );
+    this.weatherService
+      .getLocationCoordinates(this.locationName)
+      .subscribe((data: any) => {
         console.log(data[0].lat, data[0].lon);
         this.getWeather(data[0].lat, data[0].lon, '10');
-      }
-    )
+      });
   }
 
-  getWeather(lat: string, lon: string, cnt: string){
-
-    this.weatherService.getWeatherForecast(lat, lon, cnt).subscribe((data: any)=> {
-      console.log(data.list[0]);
-      this.weatherInfo.temperature = (data.list[0].main.temp-this.kelvinScale).toPrecision(2);
-      this.weatherInfo.felt_temperature = (data.list[0].main.feels_like-this.kelvinScale).toPrecision(2);
-      this.weatherInfo.pressure = (data.list[0].main.pressure);
-      this.weatherInfo.humidity = (data.list[0].main.humidity);
-      this.weatherInfo.weather = data.list[0].weather[0].main;
-      this.weatherInfo.weather_desc = data.list[0].weather[0].description;
-      this.weatherInfo.wind_speed = (data.list[0].wind.speed);
-    })
+  getWeather(lat: string, lon: string, cnt: string) {
+    this.weatherService
+      .getWeatherForecast(lat, lon, cnt)
+      .subscribe((data: any) => {
+        console.log(data.list[0]);
+        this.weatherInfo.temperature = (
+          data.list[0].main.temp - this.kelvinScale
+        ).toPrecision(2);
+        this.weatherInfo.felt_temperature = (
+          data.list[0].main.feels_like - this.kelvinScale
+        ).toPrecision(2);
+        this.weatherInfo.pressure = data.list[0].main.pressure;
+        this.weatherInfo.humidity = data.list[0].main.humidity;
+        this.weatherInfo.weather = data.list[0].weather[0].main;
+        this.weatherInfo.weather_desc = data.list[0].weather[0].description;
+        this.weatherInfo.wind_speed = data.list[0].wind.speed;
+      });
   }
 }
 
